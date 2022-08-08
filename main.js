965ef22598e16b3735aa933c319ed15b418196cb
nishchit pro coder <99675062+Nishchit-pro-coder@users.noreply.github.com>
2022-08-08 20:52:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('mustache_photo.png')
+}
